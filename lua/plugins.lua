dec0866db0e91ebfe994140eda9d374bf11509c4
Platy <daryl.limkz@gmail.com>
2021-12-31 15:46:15 +0800

    feat: some bug fixes
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -101,7 +101,11 @@ return packer.startup(function(use)
         requires = {
             'nvim-treesitter/nvim-treesitter-refactor',
             'p00f/nvim-ts-rainbow',
-            { 'windwp/nvim-autopairs', config = getConfig('autopair') },
+            {
+                'windwp/nvim-autopairs',
+                event = 'InsertCharPre',
+                config = getConfig('autopair')
+            },
         },
         config = getConfig('treesitter'),
     }
@@ -242,9 +246,9 @@ return packer.startup(function(use)
     }
 
     use {
-        'mhartington/formatter.nvim',
-        cmd = 'Format',
-        config = getConfig('format'),
+        'jose-elias-alvarez/null-ls.nvim',
+        event = 'BufReadPre',
+        config = getConfig('nullls'),
     }
 
     use { 'vim-scripts/LargeFile' }
