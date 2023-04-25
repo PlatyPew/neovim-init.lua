fa65ddfbd45a546f3bee704b002b4b48daf739d1
Platy <daryl.limkz@gmail.com>
2023-04-25 12:22:02 +0800

    refactor: adjusted mason-lspconfig handlers
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,11 +6,11 @@ local servers = {
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 capabilities.offsetEncoding = { "utf-16" }
 
 if require("jit").os == "Linux" and require("jit").arch == "arm64" then
-    require('lspconfig').clangd.setup({
+    require("lspconfig").clangd.setup({
         capabilities = capabilities,
     })
 else
@@ -20,27 +20,25 @@ end
 require("neodev").setup()
 
 -- Setup mason so it can manage external tooling
-require('mason').setup()
+require("mason").setup()
 
 -- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
+local mason_lspconfig = require("mason-lspconfig")
 
-mason_lspconfig.setup {
+mason_lspconfig.setup({
     ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-    function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            settings = servers[server_name],
-            on_attach = function(client)
-                client.server_capabilities.documentFormattingProvider = false
-                client.server_capabilities.documentRangeFormattingProvider = false
-            end,
-        }
-    end,
-    --[[ ["jdtls"] = function()
+    handlers = {
+        function(server_name)
+            require("lspconfig")[server_name].setup({
+                capabilities = capabilities,
+                settings = servers[server_name],
+                on_attach = function(client)
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end,
+            })
+        end,
+        --[[ ["jdtls"] = function()
         require('lspconfig').jdtls.setup({
             init_options = {
                 bundles = {
@@ -60,7 +58,8 @@ mason_lspconfig.setup_handlers {
             end,
         })
     end, ]]
-}
+    },
+})
 
 vim.fn.sign_define(
     "DiagnosticSignError",
