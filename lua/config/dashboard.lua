73d86fcc32ed2397c870eb623744f337a943310a
Platy <daryl.limkz@gmail.com>
2023-05-06 10:39:40 +0800

    fix: dashboard misaligned
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -16,27 +16,27 @@ vim.g.dashboard_custom_header = {
 
 vim.g.dashboard_custom_section = {
     a = {
-        description = { "  New File                  e" },
+        description = { "  New File                  e" },
         command = ":enew",
     },
     b = {
-        description = { "  Find files              ,ff" },
+        description = { "  Find files              ,ff" },
         command = ":lua require'fzf-lua'.files({cmd='rg --files --hidden --no-ignore-vcs -g \"!.git/*\"'})",
     },
     c = {
-        description = { "󰊄  Find Word               ,fr" },
+        description = { "  Find Word               ,fr" },
         command = ":lua require'fzf-lua'.live_grep_native()",
     },
     d = {
-        description = { "  Find Marks              ,fm" },
+        description = { "  Find Marks              ,fm" },
         command = ":lua require'fzf-lua'.marks()",
     },
     e = {
-        description = { "  File Explorer            ,o" },
+        description = { "  File Explorer            ,o" },
         command = ":NvimTreeToggle",
     },
     f = {
-        description = { "󰗼  Exit                      q" },
+        description = { "  Exit                      q" },
         command = ":exit",
     },
 }
