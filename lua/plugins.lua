603da5033b32ee622c5257508cd6845472b88fc1
Platy <daryl.limkz@gmail.com>
2023-12-06 10:55:11 +0800

    feat: added nvim spider
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -429,4 +429,14 @@ return require("lazy").setup({
         opts = {},
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
+
+    {
+        "chrisgrieser/nvim-spider",
+        keys = {
+            { "w", "<Cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+            { "e", "<Cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+            { "b", "<Cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+            { "ge", "<Cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+        },
+    },
 })
