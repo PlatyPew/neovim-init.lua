005770adea65d1dff86b39840310361c553ced9b
Platy <daryl.limkz@gmail.com>
2024-06-12 13:23:05 +0800

    feat: added diffview to whichkey

--- lua/functions.lua
+++ lua/functions.lua
@@ -22,3 +22,14 @@ function _G.InstantStopServer()
     vim.api.nvim_exec("InstantStopServer")
     vim.cmd("InstantStatus")
 end
+
+-- Diffview
+function _G.DiffviewToggle()
+    local lib = require("diffview.lib")
+    local view = lib.get_current_view()
+    if view then
+        vim.cmd.DiffviewClose()
+    else
+        vim.cmd.DiffviewOpen()
+    end
+end
