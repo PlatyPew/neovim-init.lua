302994d539df5f3668be7188f52bda23524249e4
Platy <daryl.limkz@gmail.com>
2024-06-13 22:29:57 +0800

    refactor: use shorthands
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -35,55 +35,55 @@ autocmd("FileType", {
 })
 
 -- Dashboard
-local dashboard = vim.api.nvim_create_augroup("dashboard_au", {})
-vim.api.nvim_create_autocmd("User", {
+local dashboard = augroup("dashboard_au", {})
+autocmd("User", {
     pattern = "DashboardReady",
     command = "nnoremap <buffer> i <Cmd>enew<CR>i",
     group = dashboard,
 })
-vim.api.nvim_create_autocmd(
+autocmd(
     "User",
     { pattern = "DashboardReady", command = "nnoremap <buffer> e <Cmd>enew<CR>", group = dashboard }
 )
-vim.api.nvim_create_autocmd(
+autocmd(
     "User",
     { pattern = "DashboardReady", command = "nnoremap <buffer> q <Cmd>exit<CR>", group = dashboard }
 )
 
 -- Null-ls
-local null_ls = vim.api.nvim_create_augroup("null-ls_au", {})
-vim.api.nvim_create_autocmd("BufReadPost", {
+local null_ls = augroup("null-ls_au", {})
+autocmd("BufReadPost", {
     pattern = "*",
     command = "lua local nls = require('null-ls'); nls.enable({ name = 'cspell', method = nls.methods.DIAGNOSTICS })",
     group = null_ls,
 })
 
 -- Terminal
-local term = vim.api.nvim_create_augroup("term_au", {})
-vim.api.nvim_create_autocmd("TermOpen", {
+local term = augroup("term_au", {})
+autocmd("TermOpen", {
     pattern = "*",
     command = "setlocal nonumber norelativenumber",
     group = term,
 })
-vim.api.nvim_create_autocmd("WinEnter", {
+autocmd("WinEnter", {
     pattern = "term://*",
     command = "nohlsearch",
     group = term,
 })
-vim.api.nvim_create_autocmd("WinEnter", {
+autocmd("WinEnter", {
     pattern = "term://*",
     command = "startinsert",
     group = term,
 })
-vim.api.nvim_create_autocmd("TermOpen", {
+autocmd("TermOpen", {
     pattern = "*",
     command = "setlocal listchars= | set nocursorline | set nocursorcolumn",
     group = term,
 })
 
 -- Fix nvim tree lazy load but override netrw
-local nvimtree = vim.api.nvim_create_augroup("nvimtree_au", {})
-vim.api.nvim_create_autocmd("UIEnter", {
+local nvimtree = augroup("nvimtree_au", {})
+autocmd("UIEnter", {
     pattern = "*",
     callback = function()
         if vim.fn.isdirectory(vim.fn.expand("%:p")) == 1 then
