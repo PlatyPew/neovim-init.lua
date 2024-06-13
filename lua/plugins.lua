e93d177aa4b52aeca768356574cc1d108b463c45
Platy <daryl.limkz@gmail.com>
2024-06-13 17:34:13 +0800

    feat: made emmet not disappear from lazy-lock

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -322,17 +322,20 @@ return require("lazy").setup({
 
     {
         "mattn/emmet-vim",
-        event = "InsertEnter",
-        cond = function()
-            local ft = { "html", "css", "markdown", "javascriptreact", "typescriptreact" }
-            local buf_ft = vim.api.nvim_buf_get_name(0):match("[^/]+%.(.-)$")
-            for _, value in ipairs(ft) do
-                if value == buf_ft then
-                    return true
-                end
-            end
-            return false
-        end,
+        keys = {
+            { "<C-Y>,", "<Plug>(emmet-expand-abbr)", mode = { "i", "n", "v" } },
+            { "<C-Y>/", "<Plug>(emmet-toggle-comment)", mode = { "i", "n" } },
+            { "<C-Y>;", "<Plug>(emmet-expand-word)", mode = { "i", "n" } },
+            { "<C-Y>D", "<Plug>(emmet-balance-tag-outword)", mode = { "i", "n", "v" } },
+            { "<C-Y>I", "<Plug>(emmet-image-encode)", mode = { "i", "n" } },
+            { "<C-Y>a", "<Plug>(emmet-anchorize-url)", mode = { "i", "n" } },
+            { "<C-Y>d", "<Plug>(emmet-balance-tag-inward)", mode = { "i", "n", "v" } },
+            { "<C-Y>i", "<Plug>(emmet-image-size)", mode = { "i", "n" } },
+            { "<C-Y>j", "<Plug>(emmet-split-join-tag)", mode = { "i", "n" } },
+            { "<C-Y>k", "<Plug>(emmet-merge-lines)", mode = { "i", "n" } },
+            { "<C-Y>k", "<Plug>(emmet-remove-tag)", mode = { "i", "n" } },
+            { "<C-Y>u", "<Plug>(emmet-update-tag)", mode = { "i", "n" } },
+        },
     },
 
     { "mbbill/undotree", cmd = "UndotreeToggle" },
