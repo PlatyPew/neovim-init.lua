475096d38d0ecac29afb53093a2fe65d98d02136
Platy <daryl.limkz@gmail.com>
2024-06-13 23:44:34 +0800

    fix: lualine being derp
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,24 +1,5 @@
 return {
     {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
-            presets = { long_message_to_split = true },
-        },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            { "stevearc/dressing.nvim", opts = {} },
-        },
-    },
-
-    {
         "altermo/ultimate-autopair.nvim",
         event = { "InsertEnter", "CmdlineEnter" },
         branch = "v0.6",
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,84 +1,107 @@
 return {
     {
+        "folke/noice.nvim",
+        opts = {
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            presets = { long_message_to_split = true },
+        },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            { "stevearc/dressing.nvim", opts = {} },
+        },
+    },
+
+    {
         "nvim-lualine/lualine.nvim",
         event = "UIEnter",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        opts = {
-            options = {
-                icons_enabled = true,
-                theme = "catppuccin",
-                section_separators = { left = "", right = "" },
-                component_separators = { left = "", right = "" },
-                globalstatus = true,
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch", "diff" },
-                lualine_c = { "filename", "filesize" },
-                lualine_x = {
-                    {
-                        require("noice").api.status.mode.get,
-                        cond = require("noice").api.status.mode.has,
-                        color = { fg = vim.g.catppuccin_overlay0 },
-                    },
-                    "%S",
-                    "location",
-                    {
-                        "filetype",
-                        colored = true,
-                    },
+        dependencies = {
+            "folke/noice.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    icons_enabled = true,
+                    theme = "catppuccin",
+                    section_separators = { left = "", right = "" },
+                    component_separators = { left = "", right = "" },
+                    globalstatus = true,
                 },
-                lualine_y = {
-                    {
-                        "encoding",
-                        padding = { left = 1, right = 0 },
-                    },
-                    "fileformat",
-                },
-                lualine_z = {
-                    {
-                        "diagnostics",
-                        sources = { "nvim_diagnostic" },
-                        symbols = { error = " ", warn = " ", info = " " },
-                        diagnostics_color = {
-                            error = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_red },
-                            warn = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_peach },
-                            info = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_yellow },
-                            hint = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_sky },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff" },
+                    lualine_c = { "filename", "filesize" },
+                    lualine_x = {
+                        {
+                            require("noice").api.status.mode.get,
+                            cond = require("noice").api.status.mode.has,
+                            color = { fg = vim.g.catppuccin_overlay0 },
+                        },
+                        "%S",
+                        "location",
+                        {
+                            "filetype",
+                            colored = true,
                         },
                     },
-                },
-            },
-            tabline = {
-                lualine_a = {
-                    {
-                        "buffers",
-                        buffers_color = {
-                            inactive = {
-                                bg = vim.g.catppuccin_surface0,
-                                fg = vim.g.catppuccin_text,
+                    lualine_y = {
+                        {
+                            "encoding",
+                            padding = { left = 1, right = 0 },
+                        },
+                        "fileformat",
+                    },
+                    lualine_z = {
+                        {
+                            "diagnostics",
+                            sources = { "nvim_diagnostic" },
+                            symbols = { error = " ", warn = " ", info = " " },
+                            diagnostics_color = {
+                                error = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_red },
+                                warn = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_peach },
+                                info = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_yellow },
+                                hint = { bg = vim.g.catppuccin_base, fg = vim.g.catppuccin_sky },
                             },
                         },
-                        padding = 1,
-                        filetype_names = {
-                            dashboard = "󰕮 Dashboard",
-                            NvimTree = " NvimTree",
-                            mason = "󰇚 Mason",
-                            undotree = " UndoTree",
-                            lspinfo = " LSP Info",
-                            lazy = "󰒲 Lazy",
-                            harpoon = "󱡅 Harpoon",
-                            oil = "󰏇 Oil",
-                        },
                     },
                 },
-                lualine_y = {
-                    function()
-                        return [[buffers]]
-                    end,
+                tabline = {
+                    lualine_a = {
+                        {
+                            "buffers",
+                            buffers_color = {
+                                inactive = {
+                                    bg = vim.g.catppuccin_surface0,
+                                    fg = vim.g.catppuccin_text,
+                                },
+                            },
+                            padding = 1,
+                            filetype_names = {
+                                dashboard = "󰕮 Dashboard",
+                                NvimTree = " NvimTree",
+                                mason = "󰇚 Mason",
+                                undotree = " UndoTree",
+                                lspinfo = " LSP Info",
+                                lazy = "󰒲 Lazy",
+                                harpoon = "󱡅 Harpoon",
+                                oil = "󰏇 Oil",
+                            },
+                        },
+                    },
+                    lualine_y = {
+                        function()
+                            return [[buffers]]
+                        end,
+                    },
                 },
-            },
-            extensions = { "nvim-tree", "nvim-dap-ui", "quickfix" },
-        },
+                extensions = { "nvim-tree", "nvim-dap-ui", "quickfix" },
+            })
+        end,
     },
 }
