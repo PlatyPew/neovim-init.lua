47d9672e367be0b3b56108607b28540b2e8b218c
Platy <daryl.limkz@gmail.com>
2024-06-15 16:13:02 +0800

    refactor: used which-key properly now
    and also changed leader to comma

--- init.lua
+++ init.lua
@@ -29,8 +29,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ","
+vim.g.maplocalleader = " "
 
 require("lazy").setup("plugins", {
     change_detection = {
