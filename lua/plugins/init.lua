3f47a05a5c61cb31df8449f967a58e8c063ced6f
Platy <daryl.limkz@gmail.com>
2024-06-15 10:48:21 +0800

    refactor: mappings for vim-doge

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -51,6 +51,9 @@ return {
         "kkoomen/vim-doge",
         build = ":call doge#install()",
         cmd = "DogeGenerate",
+        keys = {
+            { "<Leader>D", "<Cmd>DogeGenerate<CR>", desc = "Generate Docs" },
+        },
     },
 
     {
