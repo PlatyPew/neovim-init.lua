0d6143df499ecbd2c30416a8c680159cfdf6befb
Platy <daryl.limkz@gmail.com>
2024-06-15 21:20:45 +0800

    feat: updated dashboard to latest version

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,61 +1,98 @@
 return {
     {
         "nvimdev/dashboard-nvim",
-        commit = "a36b3232c98616149784f2ca2654e77caea7a522",
-        event = "VimEnter",
-        cmd = "Dashboard",
+        lazy = false,
         cond = function()
             return vim.api.nvim_buf_get_name(0) == ""
         end,
-        config = function()
-            vim.bo.buftype = "nofile"
+        opts = function()
+            local logo = [[
+         ▄▄                     ▄▄      
+  ▄█▄   ▐█▌    ██     ▀█▄  ████████████▌
+   ▀██  ▐█▌  ▄██        ▀   ▄▄▄▄██▄▄▄▄▄ 
+     ▀  ▐█▌                ▐██  ██   █▌ 
+ ▀▀▀▀▀██▀▀▀██▀▀▀▀▀   ▀▀▀█  ▐██▄▄██▄▄▄█▌ 
+      █▌  ▐█▌          ▐█     ████ ▄▄   
+     ██   ▐█▌          ▐█  ▄██  ▐█  ▀██▄
+   ██▀    ▐█▌    ██    ▄██▄     ▐█      
+▐██▀       ███████▀  ▀█▀  ▀▀▀▀█████████▀
+            ]]
 
-            vim.g.dashboard_custom_header = {
-                "         ▄▄                     ▄▄      ",
-                "  ▄█▄   ▐█▌    ██     ▀█▄  ████████████▌",
-                "   ▀██  ▐█▌  ▄██        ▀   ▄▄▄▄██▄▄▄▄▄ ",
-                "     ▀  ▐█▌                ▐██  ██   █▌ ",
-                " ▀▀▀▀▀██▀▀▀██▀▀▀▀▀   ▀▀▀█  ▐██▄▄██▄▄▄█▌ ",
-                "      █▌  ▐█▌          ▐█     ████ ▄▄   ",
-                "     ██   ▐█▌          ▐█  ▄██  ▐█  ▀██▄",
-                "   ██▀    ▐█▌    ██    ▄██▄     ▐█      ",
-                "▐██▀       ███████▀  ▀█▀  ▀▀▀▀█████████▀",
-                "                                        ",
-                "           Better Than VSCode           ",
+            logo = string.rep("\n", 8) .. logo .. "\n" .. "Better Than VSCode" .. "\n\n"
+
+            local opts = {
+                theme = "doom",
+                hide = { statusline = false },
+                config = {
+                    header = vim.split(logo, "\n"),
+                    center = {
+                        {
+                            action = "enew | startinsert",
+                            desc = " New File",
+                            icon = " ",
+                            key = "i",
+                        },
+                        {
+                            action = "lua require'fzf-lua'.files({cmd='rg --files --hidden --no-ignore-vcs -g \"!.git/*\"'})",
+                            desc = " Find Files",
+                            icon = " ",
+                            key = "f",
+                        },
+                        {
+                            action = "FzfLua live_grep_native",
+                            desc = " Find Text",
+                            icon = " ",
+                            key = "r",
+                        },
+                        {
+                            action = "FzfLua marks",
+                            desc = " Find Marks",
+                            icon = " ",
+                            key = "m",
+                        },
+                        {
+                            action = "Oil --float",
+                            desc = " File Explorer",
+                            icon = " ",
+                            key = "o",
+                        },
+                        {
+                            action = "Neogit",
+                            desc = " Git Interface",
+                            icon = " ",
+                            key = "g",
+                        },
+                        {
+                            action = "quitall",
+                            desc = " Quit",
+                            icon = " ",
+                            key = "q",
+                        },
+                    },
+                    footer = function()
+                        local stats = require("lazy").stats()
+                        return {
+                            string.format(
+                                "⚡ Neovim loaded %d/%d plugins in %.2fms",
+                                stats.loaded,
+                                stats.count,
+                                stats.startuptime
+                            ),
+                        }
+                    end,
+                },
             }
 
-            vim.g.dashboard_custom_section = {
-                a = {
-                    description = { "  New File                  e" },
-                    command = ":enew",
-                },
-                b = {
-                    description = { "  Find files              ,ff" },
-                    command = ":lua require'fzf-lua'.files({cmd='rg --files --hidden --no-ignore-vcs -g \"!.git/*\"'})",
-                },
-                c = {
-                    description = { "  Find Word               ,fr" },
-                    command = ":lua require'fzf-lua'.live_grep_native()",
-                },
-                d = {
-                    description = { "  Find Marks              ,fm" },
-                    command = ":lua require'fzf-lua'.marks()",
-                },
-                e = {
-                    description = { "  File Explorer            ,o" },
-                    command = ":NvimTreeToggle",
-                },
-                f = {
-                    description = { "  Exit                      q" },
-                    command = ":exit",
-                },
-            }
-
-            vim.g.dashboard_custom_footer = { "Stay Hydrated Folks" }
+            for _, button in ipairs(opts.config.center) do
+                button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+                button.key_format = "  %s"
+            end
 
             local hl = vim.api.nvim_set_hl
             hl(0, "DashboardHeader", { fg = vim.g.catppuccin_yellow })
             hl(0, "DashboardFooter", { fg = vim.g.catppuccin_peach })
+
+            return opts
         end,
     },
 }
