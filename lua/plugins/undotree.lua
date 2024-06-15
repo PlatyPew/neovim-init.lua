e6028d3dead152040bb0bb87b5224eb26ec8d99d
Platy <daryl.limkz@gmail.com>
2024-06-15 12:01:48 +0800

    refactor: mappings for undotree again

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,6 +4,7 @@ return {
         cmd = "UndotreeToggle",
         keys = {
             { "<Tab>", "<Cmd>UndotreeToggle<CR>", desc = "Toggle Undotree" },
+            { "<Leader>u", "<Cmd>UndotreeToggle<CR>", desc = "Toggle UndoTree" },
         },
         init = function()
             vim.o.undodir = vim.fn.stdpath("cache") .. "/undotree"
