ed439bb1ec18fbf167d32586379342b7106c3acb
Platy <daryl.limkz@gmail.com>
2024-06-15 10:54:41 +0800

    refactor: mappings for sudawrite

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,6 +31,9 @@ return {
     {
         "lambdalisue/suda.vim",
         cmd = { "SudaWrite", "SudaRead" },
+        keys = {
+            { "<Leader>W", "<Cmd>SudaWrite<CR>", desc = "Sudo Write" },
+        },
         init = function()
             vim.api.nvim_create_user_command("W", "SudaWrite", {})
         end,
