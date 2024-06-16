72a2dada37b3d3d44a5c9776a24de141b841d13b
Platy <daryl.limkz@gmail.com>
2024-06-16 20:41:00 +0800

    feat: gitsigns staged icons

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,6 +12,14 @@ return {
                 changedelete = { text = "║" },
                 untracked = { text = "┆" },
             },
+            signs_staged = {
+                add = { text = "│" },
+                change = { text = "│" },
+                delete = { text = "│" },
+                topdelete = { text = "║" },
+                changedelete = { text = "║" },
+                untracked = { text = "┆" },
+            },
             numhl = true,
         },
         config = function(_, opts)
