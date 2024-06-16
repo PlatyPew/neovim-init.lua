b2733bff0314eb8c081f431b2abe3e354474af09
Platy <daryl.limkz@gmail.com>
2024-06-16 21:56:45 +0800

    feat: change incolla to img-clip

---
+++ lua/plugins/img-clip.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "HakonHarnes/img-clip.nvim",
+        cmd = "PasteImage",
+        keys = {
+            { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste Image" },
+        },
+    },
+}
