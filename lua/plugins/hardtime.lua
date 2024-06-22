25f665896f46b1fe2fdfc2bb6272dd2e720c927b
Platy <daryl.limkz@gmail.com>
2024-06-22 20:22:53 +0800

    feat: giving myself a hard time round 2
    Signed-off-by: Platy <daryl.limkz@gmail.com>

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        cond = function()
+            return os.getenv("USER") == "daryllim"
+        end,
+        opts = {
+            max_count = 3,
+            max_time = 1000,
+            disable_mouse = false,
+            disabled_keys = {
+                ["<Up>"] = {},
+                ["<Down>"] = {},
+                ["<Left>"] = {},
+                ["<Right>"] = {},
+                ["<Space>"] = { "n", "x" },
+            },
+            disabled_filetypes = {
+                "qf",
+                "lazy",
+                "mason",
+                "oil",
+                "dashboard",
+            },
+        },
+    },
+}
