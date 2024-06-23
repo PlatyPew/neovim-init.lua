fe81a4f5ec086ce40b937e59a84c51bbdc08b997
Platy <daryl.limkz@gmail.com>
2024-06-23 22:35:34 +0800

    fix: more opaque and bigger font
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/neovide/init.lua
+++ lua/neovide/init.lua
@@ -1,6 +1,6 @@
-vim.o.guifont = "Iosevka Nerd Font Propo:h16"
+vim.o.guifont = "Iosevka Nerd Font Propo:h18"
 
-vim.g.neovide_transparency = 0.9
+vim.g.neovide_transparency = 0.95
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 
