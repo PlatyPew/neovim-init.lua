abad216462a7745314ccbf2e3e01d08a1baeff5d
Platy <daryl.limkz@gmail.com>
2024-06-23 10:57:36 +0800

    refactor: moved vim-visual to editing.lua

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -33,4 +33,13 @@ return {
         },
         opts = {},
     },
+
+    {
+        "mg979/vim-visual-multi",
+        -- stylua: ignore
+        keys = {
+            { "<C-N>", "<Plug>(VM-Find-Under)" },
+            { "<C-N>", "<Plug>(VM-Find-Subword-Under)", mode = "v" },
+        },
+    },
 }
