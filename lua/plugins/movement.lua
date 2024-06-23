2d96ac21ee217efcb21c58118aa4323cb7961526
Platy <daryl.limkz@gmail.com>
2024-06-23 10:56:32 +0800

    refactor: moved harpoon to movement.lua

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -89,4 +89,24 @@ return {
             })
         end,
     },
+
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = "nvim-lua/plenary.nvim",
+        lazy = true,
+        -- stylua: ignore
+        keys = {
+            { "mm", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon UI", },
+            { "mh", function() require("harpoon"):list():add() end, desc = "Harpoon Add", },
+            { "]m", function() require("harpoon"):list():prev() end, desc = "Harpoon Previous", },
+            { "[m", function() require("harpoon"):list():next() end, desc = "Harpoon Next", },
+
+            { "<Leader>mm", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon UI", },
+            { "<Leader>mh", function() require("harpoon"):list():add() end, desc = "Harpoon Add", },
+            { "<Leader>m]", function() require("harpoon"):list():prev() end, desc = "Harpoon Previous", },
+            { "<Leader>m[", function() require("harpoon"):list():next() end, desc = "Harpoon Next", },
+        },
+        opts = {},
+    },
 }
