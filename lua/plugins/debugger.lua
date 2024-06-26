60905291b5679f64c1ead5c7dae50ecd114bb65b
Platy <daryl.limkz@gmail.com>
2024-06-27 07:17:17 +0800

    refactor: removed java debug for port 8000
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -126,13 +126,6 @@ return {
                     vmArgs = "" .. "-Xmx2g ",
                 },
                 {
-                    name = "Debug Attach (8000)",
-                    type = "java",
-                    request = "attach",
-                    hostName = "127.0.0.1",
-                    port = 8000,
-                },
-                {
                     name = "Debug Attach (5005)",
                     type = "java",
                     request = "attach",
