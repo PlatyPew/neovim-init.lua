4dc84c9d54ba05b7228599b58cacc053418fc71a
Platy <daryl.limkz@gmail.com>
2024-06-26 09:21:11 +0800

    feat: installed dial nvim

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -96,4 +96,14 @@ return {
             end,
         },
     },
+
+    {
+        "monaqa/dial.nvim",
+        keys = {
+            { "<C-a>", "<Plug>(dial-increment)", mode = { "n", "x" } },
+            { "<C-x>", "<Plug>(dial-decrement)", mode = { "n", "x" } },
+            { "g<C-a>", "g<Plug>(dial-increment)", mode = { "n", "x" } },
+            { "g<C-x>", "g<Plug>(dial-decrement)", mode = { "n", "x" } },
+        },
+    },
 }
