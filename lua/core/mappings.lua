2b3b1f651242f38bcf72cf577a654bc9d8a63bd7
Platy <daryl.limkz@gmail.com>
2024-06-30 22:36:15 +0800

    refactor: removed unnecessary colon

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -23,20 +23,20 @@ remap("n", ";", ":")
 remap("v", ";", ":")
 
 -- Cycling buffers
-remap("n", "<Leader>bo", "<Cmd>:enew<CR>", { desc = "Open New Buffer" })
-remap("n", "<Leader>bh", "<Cmd>:bfirst<CR>", { desc = "Cycle First Buffer" })
-remap("n", "<Leader>bj", "<Cmd>:bnext<CR>", { desc = "Cycle Next Buffer" })
-remap("n", "<Leader>bk", "<Cmd>:bprevious<CR>", { desc = "Cycle Previous Buffer" })
-remap("n", "<Leader>bl", "<Cmd>:blast<CR>", { desc = "Cycle Last Buffer" })
-remap("n", "<Leader>bq", "<Cmd>bdelete<CR>", { desc = "Delete Buffer" })
+remap("n", "<Leader>bo", "<Cmd>enew<CR>", { desc = "Open New Buffer" })
+remap("n", "<Leader>bh", "<Cmd>bfirst<CR>", { desc = "Cycle First Buffer" })
+remap("n", "<Leader>bj", "<Cmd>bnext<CR>", { desc = "Cycle Next Buffer" })
+remap("n", "<Leader>bk", "<Cmd>bprevious<CR>", { desc = "Cycle Previous Buffer" })
+remap("n", "<Leader>bl", "<Cmd>blast<CR>", { desc = "Cycle Last Buffer" })
+remap("n", "<Leader>bq", "<Cmd>bdelete!<CR>", { desc = "Delete Buffer" })
 
 -- Quickfix lists
-remap("n", "<Leader>qo", "<Cmd>:copen<CR>", { desc = "Open Quickfix List" })
-remap("n", "<Leader>qh", "<Cmd>:cfirst<CR>", { desc = "Cycle First Quickfix List" })
-remap("n", "<Leader>qh", "<Cmd>:cnext<CR>", { desc = "Cycle Next Quickfix List" })
-remap("n", "<Leader>qk", "<Cmd>:cprevious<CR>", { desc = "Cycle Previous Quickfix List" })
-remap("n", "<Leader>ql", "<Cmd>:clast<CR>", { desc = "Cycle Last Quickfix List" })
-remap("n", "<Leader>qq", "<Cmd>:cclose<CR>", { desc = "Close Quickfix List" })
+remap("n", "<Leader>qo", "<Cmd>copen<CR>", { desc = "Open Quickfix List" })
+remap("n", "<Leader>qh", "<Cmd>cfirst<CR>", { desc = "Cycle First Quickfix List" })
+remap("n", "<Leader>qh", "<Cmd>cnext<CR>", { desc = "Cycle Next Quickfix List" })
+remap("n", "<Leader>qk", "<Cmd>cprevious<CR>", { desc = "Cycle Previous Quickfix List" })
+remap("n", "<Leader>ql", "<Cmd>clast<CR>", { desc = "Cycle Last Quickfix List" })
+remap("n", "<Leader>qq", "<Cmd>cclose<CR>", { desc = "Close Quickfix List" })
 
 -- Stops cursor from flying everywhere
 remap("n", "n", "nzzzv")
