7aa98a31bed20b3a9e63307d2d4e86b70e65e1be
Platy <daryl.limkz@gmail.com>
2024-07-10 16:25:03 +0800

    fix: force osc52 usage
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -37,3 +37,16 @@ if vim.fn.isdirectory(vim.fn.stdpath("data") .. "/venv") == 0 then
 end
 
 vim.g.python3_host_prog = vim.fn.stdpath("data") .. "/venv/bin/python"
+
+-- Clipboard
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+}
