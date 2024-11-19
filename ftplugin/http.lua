0ad52f72287b2160bcdad5fc5f420dc8c4436f6c
Platy <daryl.limkz@gmail.com>
2024-11-20 06:23:56 +0800

    feat: added kulala mappings
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,6 +1,6 @@
 local wk = require("which-key")
 
-wk.register({
+wk.add({
     h = { name = "Kulala" },
 }, { mode = "n", prefix = "<Leader>" })
 
@@ -12,4 +12,10 @@ remap("n", "]h", function() require("kulala").jump_next() end, { desc = "Next HT
 remap("n", "<Leader>h[", function() require("kulala").jump_prev() end, { desc = "Previous HTTP Request" })
 remap("n", "<Leader>h]", function() require("kulala").jump_next() end, { desc = "Next HTTP Request" })
 remap("n", "<Leader>hr", function() require('kulala').run() end, { desc = "Run HTTP Request" })
+remap("n", "<Leader>hR", function() require('kulala').run_all() end, { desc = "Run All HTTP Requests" })
+remap("n", "<Leader>hi", function() require('kulala').inspect() end, { desc = "Inspect HTTP Request" })
+remap("n", "<Leader>hs", function() require('kulala').show_stats() end, { desc = "Show HTTP Request Stats" })
+remap("n", "<Leader>hc", function() require('kulala').copy() end, { desc = "Copy HTTP Request as cURL" })
+remap("n", "<Leader>hC", function() require('kulala').from_curl() end, { desc = "Import from cURL" })
+remap("n", "<Leader>ht", function() require('kulala').toggle_view() end, { desc = "Toogle HTTP Body or Header" })
 -- stylua: ignore end
