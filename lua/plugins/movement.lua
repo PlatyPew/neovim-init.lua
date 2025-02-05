0648bc9edd5ee3b597c79e34018edebb74122012
Platy <daryl.limkz@gmail.com>
2025-02-05 11:07:24 +0800

    style: formatting

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -13,7 +13,7 @@ return {
         "chrisgrieser/nvim-spider",
         -- stylua: ignore
         keys = {
-            {"w", function() require("spider").motion("w") end, mode = { "n", "o", "x" }},
+            { "w", function() require("spider").motion("w") end, mode = { "n", "o", "x" } },
             { "e", function() require("spider").motion("e") end, mode = { "n", "o", "x" } },
             { "b", function() require("spider").motion("b") end, mode = { "n", "o", "x" } },
             { "ge", function() require("spider").motion("ge") end, mode = { "n", "o", "x" } },
